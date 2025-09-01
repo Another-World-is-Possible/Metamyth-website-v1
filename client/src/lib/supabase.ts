10233994d99caf43cdb80e9fc8585315a1e45ea1
Nathan <moso.nate.r@gmail.com>
2025-08-31 18:06:04 -0700

    Attempting to fix missing file errors

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase URL and Anon Key must be defined in .env file. (e.g. VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY)");
+  //throw new Error("Supabase URL and Anon Key must be defined in .env file. (e.g. VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY)");
 }
 
 // Create and export the Supabase client
